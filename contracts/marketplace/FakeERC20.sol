7619d9bef54fe924f58051a8677f2bee364609d3
Ariel Barmat <abarmat@decentraland.org>
2018-05-30 16:45:02 -0300

    chore: 2 spaces

--- contracts/marketplace/FakeERC20.sol
+++ contracts/marketplace/FakeERC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract FakeERC20 is StandardToken {
-    function setBalance(address holder, uint256 amount) public {
-        balances[holder] = amount;
-    }
+  function setBalance(address holder, uint256 amount) public {
+    balances[holder] = amount;
+  }
 }
--- contracts/marketplace/FakeERC721.sol
+++ contracts/marketplace/FakeERC721.sol
@@ -3,23 +3,23 @@ pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract FakeERC721 is ERC721Token {
-    constructor(string name, string symbol) public
-    ERC721Token(name, symbol)
-    { }
+  constructor(string name, string symbol) public
+  ERC721Token(name, symbol)
+  { }
 
-    function mint(address _to, uint256 _tokenId) public {
-        super._mint(_to, _tokenId);
-    }
+  function mint(address _to, uint256 _tokenId) public {
+    super._mint(_to, _tokenId);
+  }
 
-    function burn(uint256 _tokenId) public {
-        super._burn(ownerOf(_tokenId), _tokenId);
-    }
+  function burn(uint256 _tokenId) public {
+    super._burn(ownerOf(_tokenId), _tokenId);
+  }
 
-    function setTokenURI(uint256 _tokenId, string _uri) public {
-        super._setTokenURI(_tokenId, _uri);
-    }
+  function setTokenURI(uint256 _tokenId, string _uri) public {
+    super._setTokenURI(_tokenId, _uri);
+  }
 
-    function isAuthorized(address _spender, uint256 _tokenId) external view returns (bool) {
-        return super.isApprovedOrOwner(_spender, _tokenId);
-    }
+  function isAuthorized(address _spender, uint256 _tokenId) external view returns (bool) {
+    return super.isApprovedOrOwner(_spender, _tokenId);
+  }
 }
